9db1b9111d556f6396278bbac5101094e52cf0d8
dengyinlin <dylin@pku.edu.cn>
2020-05-25 18:33:02 +0800

    Implements DiscussionBoard.

--- pages/messageBoard/messageBoard.js
+++ pages/messageBoard/messageBoard.js
@@ -1,9 +1,6 @@
-const util = require('../../utils/util.js')
+var util = require('../../utils/util.js');
 const app = getApp()
-var mydata = {
-  end: 0,
-  replyUserName: ""
-}
+// 暂时没有实现“回复具体某条评论”，“删除评论”，“上传图片”的功能。
 Page({
 
   /**
@@ -14,6 +11,11 @@ Page({
     debug: true,
     p1: "https://ossweb-img.qq.com/images/lol/web201310/skin/big107000.jpg",
     p2: "https://ossweb-img.qq.com/images/lol/web201310/skin/big143004.jpg",
+    // page data
+    InputBottom: 0,
+    activityId: undefined,
+    currentComment: "",
+    inputValue: "",
     list: [
     {
         id:1, // 当前回复的id
@@ -22,6 +24,7 @@ Page({
         replyUserName: "Bob",
         replyUserAvatarUrl: "https://ossweb-img.qq.com/images/lol/web201310/skin/big143004.jpg",
         myCommentFlag: false,
+        is_img: false,
         content: "喵喵喵",
         insertTime: 1597852800000.0,
     },
@@ -32,6 +35,51 @@ Page({
         replyUserName: "Alice",
         replyUserAvatarUrl: "https://ossweb-img.qq.com/images/lol/web201310/skin/big107000.jpg",
         myCommentFlag: true,
+        is_img: true,
+        content: "https://ossweb-img.qq.com/images/lol/web201310/skin/big10006.jpg",
+        insertTime: 1597852900000.0,
+    },
+    {
+        id:3, // 当前回复的id
+        userName: "Alice",
+        userAvatarUrl: "https://ossweb-img.qq.com/images/lol/web201310/skin/big107000.jpg",
+        replyUserName: "Bob",
+        replyUserAvatarUrl: "https://ossweb-img.qq.com/images/lol/web201310/skin/big143004.jpg",
+        myCommentFlag: false,
+        is_img: false,
+        content: "喵喵喵",
+        insertTime: 1597852800000.0,
+    },
+    {
+        id:4, // 当前回复的id
+        userName: "Bob",
+        userAvatarUrl: "https://ossweb-img.qq.com/images/lol/web201310/skin/big143004.jpg",
+        replyUserName: "Alice",
+        replyUserAvatarUrl: "https://ossweb-img.qq.com/images/lol/web201310/skin/big107000.jpg",
+        myCommentFlag: true,
+        is_img: false,
+        content: "汪汪汪",
+        insertTime: 1597852900000.0,
+    },
+    {
+        id:5, // 当前回复的id
+        userName: "Alice",
+        userAvatarUrl: "https://ossweb-img.qq.com/images/lol/web201310/skin/big107000.jpg",
+        replyUserName: "Bob",
+        replyUserAvatarUrl: "https://ossweb-img.qq.com/images/lol/web201310/skin/big143004.jpg",
+        myCommentFlag: false,
+        is_img: false,
+        content: "喵喵喵",
+        insertTime: 1597852800000.0,
+    },
+    {
+        id:6, // 当前回复的id
+        userName: "Bob",
+        userAvatarUrl: "https://ossweb-img.qq.com/images/lol/web201310/skin/big143004.jpg",
+        replyUserName: "Alice",
+        replyUserAvatarUrl: "https://ossweb-img.qq.com/images/lol/web201310/skin/big107000.jpg",
+        myCommentFlag: true,
+        is_img: false,
         content: "汪汪汪",
         insertTime: 1597852900000.0,
     },
@@ -39,48 +87,60 @@ Page({
   },
 
   /**
+   * InputFocus: 输入框聚焦时触发
+   */
+  InputFocus(e) {
+    this.setData({
+      InputBottom: e.detail.height
+    })
+  },
+  /**
+   * InputBlur: 输入框失去焦点时触发
+   */
+  InputBlur(e) {
+    this.setData({
+      InputBottom: 0
+    })
+  },
+  /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    var that = this;
-    mydata.sourceId = options.sourceId
-    mydata.commentId = "";
-    mydata.replyUserName = "";
+  onLoad: function (e) {
+    var self = this;
+    
+    let response = JSON.parse(e.query);
+    console.log(' msg response: ', response)
+    this.setData({
+        activityID: response.activityID
+    })
+    
     //设置scroll的高度
     wx.getSystemInfo({
       success: function (res) {
-        that.setData({
+        self.setData({
           scrollHeight: res.windowHeight,
         });
       }
     });
-    mydata.page = 1;
-    that.getPageInfo(mydata.page);
+    self.getPageInfo();
   },
   /**
    * 页面下拉刷新事件的处理函数
    */
   refresh: function () {
     console.log('refresh');
-    mydata.page = 1
-    this.getPageInfo(mydata.page, function () {
-      this.setData({
-        list: []
-      })
-    });
-    mydata.end = 0;
+    this.setData({
+      list: []
+    })
+    this.getPageInfo();
   },
   /**
    * 页面上拉触底事件的处理函数
    */
   bindDownLoad: function () {
     console.log("onReachBottom");
-    var that = this;
-    if (mydata.end == 0) {
-      mydata.page++;
-      that.getPageInfo(mydata.page);
-    }
   },
+  /*
   bindReply: function (e) {
     console.log(e);
     mydata.commentId = e.target.dataset.commentid;
@@ -90,16 +150,9 @@ Page({
       reply: true
     })
   },
-  // 合并数组
-  addArr(arr1, arr2) {
-    for (var i = 0; i < arr2.length; i++) {
-      arr1.push(arr2[i]);
-    }
-    return arr1;
-  },
   deleteComment: function (e) {
     console.log(e);
-    var that = this;
+    var self = this;
     var commentId = e.target.dataset.commentid;
 
     wx.showModal({
@@ -117,7 +170,7 @@ Page({
               "content-type": "application/x-www-form-urlencoded;charset=utf-8",
             },
             success: res => {
-              that.refresh();
+              self.refresh();
               wx.showToast({
                 title: "删除成功"
               })
@@ -129,106 +182,87 @@ Page({
       }
     })
   },
-  cancleReply: function (e) {
-    mydata.commentId = "";
-    mydata.replyUserName = "";
-    this.setData({
-      replyUserName: mydata.replyUserName,
-      reply: false
-    })
-  },
+  */
   // 更新页面信息
   // 此处的回调函数在 传入新值之前执行 主要用来清除页面信息
   getPageInfo(page, callback) {
-    if (this.data.debug) return ;
+    //if (this.data.debug) return ;
     
-    var that = this;
+    var self = this;
     // This function is not implemented.
     // util.showLoading();
         
     console.log("getPageInfo");
-    console.log("page" + page);
-    var limited = 6;
-    var offset = (page - 1) * 6;
+    let aid = self.data.activityID;
     wx.request({
-      url: app.globalData.rootUrl + 'user/activityDisplayer/discussion/', 
+      url: app.globalData.rootUrl + 'user/activityDisplayer/discussion', 
       method: "POST",
       data: {
-        sourceId: mydata.sourceId,
-        limited: limited,
-        offset: offset
+        third_session: wx.getStorageSync('third_session'),
+        activity_id: JSON.stringify(aid),
       },
       header: {
         "content-type": "application/x-www-form-urlencoded;charset=utf-8",
       },
       success: res => {
+        console.log('getPageInfo returns: ')
         console.log(res);
-        if (page == 1) {
-          that.data.list = res.data;
-          that.setData({
-            list: that.data.list
-          })
-          mydata.end = 0;
-        } else {
-          // 当前页为其他页
-          var list = that.data.list;
-          if (res.data.length != 0) {
-            list = that.addArr(list, res.data);
-            that.setData({
-              list: list
-            })
-            mydata.end = 0;
-          } else {
-            mydata.end = 1;
-          }
+        
+        let list = res.data;
+        let i = 0, len = list.length;
+        for (; i < len; i++) {
+            list[i].formattedTime = util.formatTime(new Date(list[i].insertTime))
         }
-        wx.hideLoading();
+        self.setData({
+          list: list
+        })
+        wx.hideLoading();  //隐藏loading提示框
       }
     })
   },
-  submitForm(e) {
-    var form = e.detail.value;
-    var that = this;
-    console.log(app.globalData.haulUserInfo);
-    if (form.comment == "") {
-      util.showLog('请输入评论');
-      return;
+  
+  getInput: function(e) {
+      this.setData({
+          currentComment: e.detail.value
+      })
+  },
+  
+  onSubmit: function(e) {
+    let self = this;
+    let content = self.data.currentComment;
+    if (content == "") {
+      wx.showToast({
+        title: '请输入评论', 
+        icon: 'none',
+        duration:2000});
+      return ;
     }
-    // 提交评论
+    let aid = self.data.activityID;
+    let send = {
+      third_session: wx.getStorageSync('third_session'),
+      content: JSON.stringify(content),
+      activity_id: JSON.stringify(aid),
+      is_img: JSON.stringify(false)
+    }
     wx.request({
       url: app.globalData.rootUrl + 'user/activityDisplayer/discussion/create', 
       method: "POST",
-      data: {
-        sourceId: mydata.sourceId,
-        comment: form.comment,
-        userName: app.globalData.haulUserInfo.userName,
-        replyCommentId: mydata.commentId,
-        replyUserName: mydata.replyUserName,
-        userPhoto: app.globalData.haulUserInfo.userPhoto
-      },
+      data: send,
       header: {
-        "content-type": "application/x-www-form-urlencoded;charset=utf-8",
-        //token: app.globalData.token
+        'content-type': 'application/x-www-form-urlencoded',
+        'chartset': 'utf-8'
       },
       success: res => {
-        console.log(res)
-        if (res.data.success) {
-          wx.showToast({
-            title: "回复成功"
-          })
-          that.refresh();
-          mydata.commentId = "";
-          mydata.replyUserName = "";
-          this.setData({
-            replyUserName: mydata.replyUserName,
-            reply: false
-          })
-        } else {
-          wx.showToast({
-            title: '回复失败，请检查您的网络',
-          })
-        }
+        console.log(res);
+        self.data.list = res.data;
+        self.setData({
+          list: self.data.list,
+          inputValue: ''
+        })
+        wx.hideLoading();  //隐藏loading提示框
       }
     })
-  }
+      
+  },
+  
 })
